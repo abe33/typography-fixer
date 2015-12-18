8945065a24c61d1c2ac60ebcba98ca75836ad4fe
abe33 <cedric.nehemie@gmail.com>
2015-12-18 11:43:59 +0100

    Refactor all methods to only have pure functions

--- src/typographic-fixer.js
+++ src/typographic-fixer.js
@@ -1,152 +1,81 @@
-export default function typographyFixer (lang) {
-  const rulesLibrary = {}
+export function check (rules, string) {
+  if (!rules || !string) {
+    throw new Error('The check arguments are mandatory')
+  }
 
-  if (lang) {
+  let results = []
+
+  if (rules.length === 0) { return undefined }
+
+  for (let i = 0, len = rules.length; i < len; i++) {
+    const rule = rules[i]
+    results = results.concat(rule.check(string))
+  }
+
+  return results.length > 0 ? results : undefined
+}
+
+export function fix (rules, string) {
+  if (!rules || !string) {
+    throw new Error('The string argument is mandatory')
+  }
+
+  if (rules.length === 0) { return string }
+
+  for (let i = 0, len = rules.length; i < len; i++) {
+    const rule = rules[i]
+    string = rule.fix(string)
+  }
+
+  return string
+}
+
+export function group (name, rules) {
+  return rules.reduce(function(memo, el) {
+    return memo.concat(el)
+  }, []).map((rule) => {
     return {
-      rules (block) {
-        rules(lang, block)
-      },
-      hasRule (rule) {
-        return hasRule(lang, rule)
-      },
-      check (string, options = {}) {
-        return check(string, merge(options, {lang}))
-      },
-      fix (string, options = {}) {
-        return fix(string, merge(options, {lang}))
-      }
+      name: name + '.' + rule.name,
+      check: rule.check,
+      fix: rule.fix
     }
+  })
+}
+
+export function rule (name, expression, replacement) {
+  if (!name || !expression || !replacement) {
+    throw new Error('All arguments of the define function are mandatory')
+  }
+
+  let source
+
+  if (expression instanceof RegExp) {
+    source = expression.source
   } else {
-    return {rules, hasRule, check, fix}
+    source = expression
   }
 
-  function check (string, options = {}) {
-    if (!string) {
-      throw new Error('The string argument is mandatory')
+  return {
+    name,
+    check (string) {
+      const re = new RegExp(source, 'g')
+      const matches = []
+      let match
+      do {
+        match = re.exec(string)
+        if (match) {
+          matches.push({
+            rule: this.name,
+            range: [match.index, re.lastIndex]
+          })
+        }
+      } while (match)
+
+      return matches
+    },
+    fix (string) {
+      const re = new RegExp(source, 'g')
+      return string.replace(re, replacement)
     }
-
-    let {lang} = options
-    if (!lang) { lang = 'en' }
-
-    const rules = rulesLibrary[lang]
-    let results = []
-
-    if (!rules) { return undefined }
-
-    for (let i = 0, len = rules.length; i < len; i++) {
-      const rule = rules[i]
-      results = results.concat(rule.check(string))
-    }
-
-    return results.length > 0 ? results : undefined
   }
-
-  function fix (string, options = {}) {
-    if (!string) {
-      throw new Error('The string argument is mandatory')
-    }
-
-    let {lang} = options
-    if (!lang) { lang = 'en' }
-
-    const rules = rulesLibrary[lang]
-
-    if (!rules) { return string }
-
-    for (let i = 0, len = rules.length; i < len; i++) {
-      const rule = rules[i]
-      string = rule.fix(string)
-    }
-
-    return string
-  }
-
-  function rules (lang, block) {
-    if (typeof lang !== 'string') {
-      throw new Error("Can't call rules without a lang")
-    }
-
-    if (typeof block !== 'function') {
-      throw new Error("Can't call rules without a function")
-    }
-
-    const definedRules = evaluateBlock(block)
-
-    rulesLibrary[lang] = (rulesLibrary[lang] || []).concat(definedRules)
-  }
-
-  function hasRule (lang, ruleName) {
-    const langRules = rulesLibrary[lang]
-    if (!langRules) { return false }
-
-    return langRules.some((rule) => { return rule.name === ruleName })
-  }
-}
-
-function evaluateBlock (block) {
-  const rules = []
-
-  block(getRulesContext(rules, []))
-
-  return rules
-}
-
-function getRulesContext (rules, context) {
-  return {group, define}
-
-  function group (name, block) {
-    if (!name || !block) {
-      throw new Error('All arguments of the group function are mandatory')
-    }
-
-    block(getRulesContext(rules, context.concat(name)))
-  }
-
-  function define (name, expression, replacement) {
-    if (!name || !expression || !replacement) {
-      throw new Error('All arguments of the define function are mandatory')
-    }
-
-    let source
-    if (expression instanceof RegExp) {
-      source = expression.source
-    } else {
-      source = expression
-    }
-
-    const ruleName = context.concat(name).join('.')
-
-    rules.push({
-      name: ruleName,
-      check (string) {
-        const re = new RegExp(source, 'g')
-        const matches = []
-        let match
-        do {
-          match = re.exec(string)
-          if (match) {
-            matches.push({
-              rule: ruleName,
-              range: [match.index, re.lastIndex]
-            })
-          }
-        } while (match)
-
-        return matches
-      },
-      fix (string) {
-        const re = new RegExp(source, 'g')
-        return string.replace(re, replacement)
-      }
-    })
-  }
-}
-
-function merge (a, b) {
-  const o = {}
-
-  for (const key in a) { o[key] = a[key] }
-  for (const key in b) { o[key] = b[key] }
-
-  return o
 }
--- test/typographic-fixer.js
+++ test/typographic-fixer.js
@@ -1,171 +1,110 @@
 import expect from 'expect.js'
-import typo from '../src/typographic-fixer'
+import {rule, group, check, fix} from '../src/typographic-fixer'
 
 describe('typographyFixer', () => {
-  let [fixer] = []
+  describe('rule', () => {
+    let ruleObject
 
-  beforeEach(() => {
-    fixer = typo()
-  })
-
-  it('returns a new fixer object', () => {
-    expect(fixer.check).to.be.a(Function)
-    expect(fixer.fix).to.be.a(Function)
-    expect(fixer.rules).to.be.a(Function)
-  })
-
-  describe('when called with a lang as argument', () => {
-    it('returns a fixer locked on this language', () => {
-      fixer = typo('lang')
-
-      fixer.rules(({define, group}) => {
-        define('Foo', 'foo', 'bar')
-        group('bar', ({define}) => {
-          define('Da', 'Da', 'Ad')
-        })
-      })
-
-      expect(fixer.hasRule('Foo')).to.be.ok()
-      expect(fixer.hasRule('bar.Da')).to.be.ok()
-      expect(fixer.check('Da foo foo')).to.have.length(3)
-      expect(fixer.fix('Da foo foo')).to.eql('Ad bar bar')
-    })
-  })
-
-  describe('.rules', () => {
-    it('throws when called without a lang', () => {
-      expect(() => { fixer.rules() }).to.throwError()
+    beforeEach(() => {
+      ruleObject = rule('Foo', /foo/, 'bar')
     })
 
-    it('throws when called without a block', () => {
-      expect(() => { fixer.rules('lang') }).to.throwError()
+    it('returns a rule object', () => {
+      expect(ruleObject).not.to.be(undefined)
+      expect(ruleObject.name).to.eql('Foo')
+      expect(ruleObject.check).to.be.a(Function)
+      expect(ruleObject.fix).to.be.a(Function)
     })
 
-    it('calls the passed-in function with a rules object', () => {
-      let rulesObject
-      fixer.rules('lang', (rules) => {
-        rulesObject = rules
+    describe('.check', () => {
+      it('returns a report object for each match', () => {
+        const reports = ruleObject.check('Da foo foo')
+
+        expect(reports).to.eql([
+          {rule: 'Foo', range: [3, 6]},
+          {rule: 'Foo', range: [7, 10]}
+        ])
       })
 
-      expect(rulesObject).not.to.be(undefined)
-    })
+      it('returns an empty array if there is no match', () => {
+        const reports = ruleObject.check('Da bar bar')
 
-    describe('defining a rule', () => {
-      it('throws when called without all the parameters', () => {
-        fixer.rules('lang', ({define}) => {
-          expect(() => { define() }).to.throwError()
-          expect(() => { define('foo') }).to.throwError()
-          expect(() => { define('foo', 'foo') }).to.throwError()
-        })
-      })
-
-      it('makes it available in check method', () => {
-        fixer.rules('lang', ({define}) => {
-          define('Foo', /foo/, 'bar')
-        })
-
-        expect(fixer.check('Da foo foo', {lang: 'lang'})).to.have.length(2)
-        expect(fixer.check('Da bar', {lang: 'lang'})).to.be(undefined)
-      })
-
-      it('makes it available in fix method', () => {
-        fixer.rules('lang', ({define}) => {
-          define('Foo', /foo/, 'bar')
-        })
-
-        expect(fixer.fix('Da foo foo', {lang: 'lang'})).to.eql('Da bar bar')
-      })
-
-      it('makes it available only for the defined lang', () => {
-        fixer.rules('lang', ({define}) => {
-          define('Foo', /foo/, 'bar')
-        })
-
-        expect(fixer.check('Da foo foo', {lang: 'en'})).to.be(undefined)
-        expect(fixer.fix('Da foo foo', {lang: 'en'})).to.eql('Da foo foo')
-      })
-
-      describe('with a string as expression', () => {
-        it('creates a rules using the string as source for the regexp', () => {
-          fixer.rules('lang', ({define}) => {
-            define('Foo', 'foo', 'bar')
-          })
-
-          expect(fixer.check('Da foo foo', {lang: 'lang'})).to.have.length(2)
-          expect(fixer.fix('Da foo foo', {lang: 'lang'})).to.eql('Da bar bar')
-        })
-      })
-
-      describe('with a function as replacement', () => {
-        it('creates a rules using the function as a replacement function', () => {
-          fixer.rules('lang', ({define}) => {
-            define('Foo', 'foo', (s) => { return s[0] })
-          })
-
-          expect(fixer.fix('Da foo foo', {lang: 'lang'})).to.eql('Da f f')
-        })
+        expect(reports).to.eql([])
       })
     })
 
-    describe('defining a group', () => {
-      beforeEach(() => {
-        fixer.rules('lang', ({group}) => {
-          group('bar', ({define}) => {
-            define('Foo', 'foo', 'bar')
-          })
-        })
-      })
-
-      it('throws when called without all the parameters', () => {
-        fixer.rules('lang', ({group}) => {
-          expect(() => { group() }).to.throwError()
-          expect(() => { group('foo') }).to.throwError()
-        })
-      })
-
-      it('takes a function to define rules for the group', () => {
-        expect(fixer.hasRule('lang', 'bar.Foo')).to.be.ok()
-      })
-
-      it('applies the rule as any other', () => {
-        expect(fixer.fix('Da foo foo', {lang: 'lang'})).to.eql('Da bar bar')
+    describe('.fix', () => {
+      it('replaces instances of matches by the replacement string', () => {
+        expect(ruleObject.fix('Da foo foo')).to.eql('Da bar bar')
       })
     })
   })
 
-  describe('.hasRule', () => {
-    it('returns true when a rule exist for the specified lang', () => {
-      fixer.rules('lang', ({define}) => {
-        define('Foo', /foo/, 'bar')
-      })
+  describe('group', () => {
+    let ruleObject
 
-      expect(fixer.hasRule('lang', 'Foo')).to.be.ok()
+    beforeEach(() => {
+      ruleObject = rule('Foo', /foo/, 'bar')
     })
 
-    it('returns false when a rule does noe exist for the specified lang', () => {
-      fixer.rules('lang', ({define}) => {
-        define('Foo', /foo/, 'bar')
-      })
+    it('returns an array with the provided rules with a namespaced name', () => {
+      const rules = group('bar', [ruleObject])
 
-      expect(fixer.hasRule('lang', 'bar')).not.to.be.ok()
+      expect(rules).to.be.an(Array)
+      expect(rules).to.have.length(1)
+      expect(rules[0].name).to.eql('bar.Foo')
     })
 
-    it('returns false when the lang has no rule', () => {
-      expect(fixer.hasRule('lang', 'Foo')).not.to.be.ok()
+    it('flatten rules coming from nested groups', () => {
+      const rules = group('bar', [
+        group('baz', [
+          group('bat', [ruleObject])
+        ])
+      ])
+
+      expect(rules).to.be.an(Array)
+      expect(rules).to.have.length(1)
+      expect(rules[0].name).to.eql('bar.baz.bat.Foo')
+    })
+
+    describe('modified rules', () => {
+      it('checks a string but reports use the new name', () => {
+        const rules = group('bar', [ruleObject])
+        const modifiedRule = rules[0]
+        const reports = modifiedRule.check('Da foo foo')
+
+        expect(reports).to.eql([
+          {rule: 'bar.Foo', range: [3, 6]},
+          {rule: 'bar.Foo', range: [7, 10]}
+        ])
+      })
+
+      it('replaces instances of matches by the replacement string', () => {
+        const modifiedRule = group('bar', [ruleObject])[0]
+
+        expect(modifiedRule.fix('Da foo foo')).to.eql('Da bar bar')
+      })
     })
   })
 
-  describe('.check', () => {
-    it('throws when called without a string', () => {
-      expect(() => { fixer.check() }).to.throwError()
+  describe('check', () => {
+    it('throws when called without arguments', () => {
+      expect(() => { check() }).to.throwError()
+      expect(() => { check([]) }).to.throwError()
     })
 
-    it('returns a report of the broken rules with concerned ranges', () => {
-      fixer.rules('lang', ({define}) => {
-        define('Foo', /foo/, 'bar')
-      })
+    it('returns undefined when called without any rules', () => {
+      expect(check([], 'string')).to.be(undefined)
+    })
 
-      const reports = fixer.check('Da foo foo', {lang: 'lang'})
+    it('returns undefined when there is no match in the string', () => {
+      const ruleObject = rule('Foo', /foo/, 'bar')
+      expect(check([ruleObject], 'string')).to.be(undefined)
+    })
+
+    it('returns a report object for each match', () => {
+      const ruleObject = rule('Foo', /foo/, 'bar')
+      const reports = check([ruleObject], 'Da foo foo')
 
       expect(reports).to.eql([
         {rule: 'Foo', range: [3, 6]},
@@ -174,9 +113,26 @@ describe('typographyFixer', () => {
     })
   })
 
-  describe('.fix', () => {
-    it('throws when called without a string', () => {
-      expect(() => { fixer.fix() }).to.throwError()
+  describe('fix', () => {
+    it('throws when called without arguments', () => {
+      expect(() => { fix() }).to.throwError()
+      expect(() => { fix([]) }).to.throwError()
+    })
+
+    it('returns the string when called without any rules', () => {
+      expect(fix([], 'string')).to.eql('string')
+    })
+
+    it('returns the string when there is no match', () => {
+      const ruleObject = rule('Foo', /foo/, 'bar')
+
+      expect(fix([ruleObject], 'string')).to.eql('string')
+    })
+
+    it('replaces instances of matches by the replacement string', () => {
+      const ruleObject = rule('Foo', /foo/, 'bar')
+
+      expect(fix([ruleObject], 'Da foo foo')).to.eql('Da bar bar')
     })
   })
 })
