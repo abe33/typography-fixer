7233964d5a4157546f88ee0cafaae2c82f7cc4d9
abe33 <cedric.nehemie@gmail.com>
2015-12-18 20:24:34 +0100

    Add missing space rules

--- src/fr-FR.js
+++ src/fr-FR.js
@@ -3,6 +3,8 @@ import {rule, group} from './typographic-fixer'
 export default group([
   group('spaces', [
     rule('multipleSpaces', /\x20+/, ' '),
+    rule('spaceBeforePunctuation', /(?:\x20|\u00a0)?([!?:%])/, '&nbsp;$1'),
+    rule('spaceBeforePunctuation', /(?:^|\x20|\u00a0)([^&\n\s]*)(;)/, '$1&nbsp;$2'),
     rule('literalNonBreakingSpaces', /\u00a0/, '&nbsp;')
   ])
 ])
--- test/fr-FR.js
+++ test/fr-FR.js
@@ -11,5 +11,17 @@ describe('fr-FR rules', () => {
     it('replaces non-breaking spaces with the corresponding html entity', () => {
       expect(fix(rules, '17\u00a0%')).to.eql('17&nbsp;%')
     })
+
+    let charsWithNbspBefore = ['!','?',';',':','%']
+    charsWithNbspBefore.forEach((char) => {
+      it(`replaces a simple space before ${char} with a non-breaking one`, () => {
+        expect(fix(rules, `Foo ${char}`)).to.eql(`Foo&nbsp;${char}`)
+      })
+      it(`adds a non-breaking before ${char} if there is no space`, () => {
+        expect(fix(rules, `Foo${char}`)).to.eql(`Foo&nbsp;${char}`)
+      })
+    })
+  })
+
   })
 })
