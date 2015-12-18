771a5e582d2c5f4a81ee21be4c395afc103c734a
abe33 <cedric.nehemie@gmail.com>
2015-12-18 22:44:59 +0100

    :bug: Fix non breaking space added before ! in image and : in url

--- src/fr-FR.js
+++ src/fr-FR.js
@@ -10,7 +10,9 @@ export default group([
   ]),
   group('spaces', [
     rule('multipleSpaces', /\x20+/, ' '),
-    rule('spaceBeforePunctuation', /(?:\x20|\u00a0)?([!?:%])/, '&nbsp;$1'),
+    rule('spaceBeforePunctuation', /(?:\x20|\u00a0)?([?%])/, '&nbsp;$1'),
+    rule('spaceBeforePunctuation', /(?:\x20|\u00a0)?(:)(?!\/\/)/, '&nbsp;$1'),
+    rule('spaceBeforePunctuation', /(?:\x20|\u00a0)?(!)(?!\[)/, '&nbsp;$1'),
     rule('spaceBeforePunctuation', /(?:^|\x20|\u00a0)([^&\n\s]*)(;)/, '$1&nbsp;$2'),
     rule('literalNonBreakingSpaces', /\u00a0/, '&nbsp;')
   ]),
