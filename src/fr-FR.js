2d5a7244e220231801763d323cf1ce410a4a68cc
abe33 <cedric.nehemie@gmail.com>
2015-12-18 20:46:24 +0100

    Add ordinal number rules

--- src/fr-FR.js
+++ src/fr-FR.js
@@ -13,5 +13,13 @@ export default group([
     rule('spaceBeforePunctuation', /(?:\x20|\u00a0)?([!?:%])/, '&nbsp;$1'),
     rule('spaceBeforePunctuation', /(?:^|\x20|\u00a0)([^&\n\s]*)(;)/, '$1&nbsp;$2'),
     rule('literalNonBreakingSpaces', /\u00a0/, '&nbsp;')
+  ]),
+  group('ordinal', [
+    rule('greaterThan10', /(\d{2,})[èe]mes/, '$1<sup>èmes</sup>'),
+    rule('firstMalePlural', /(\d{1})ers/, '$1<sup>ers</sup>'),
+    rule('firstFemalePlural', /(\d{1})[èe]res/, '$1<sup>res</sup>'),
+    rule('lowerThan10', /(\d{1})[èe]mes/, '$1<sup>es</sup>'),
+    rule('firstFemale', /(\d)[èe]re/, '$1<sup>re</sup>'),
+    rule('firstMale', /(\d)[èe]me/, '$1<sup>e</sup>')
   ])
 ])
--- test/fr-FR.js
+++ test/fr-FR.js
@@ -66,4 +66,29 @@ describe('fr-FR rules', () => {
       expect(fix(rules, 'Mr.')).to.eql('M.')
     })
   })
+
+  describe('ordinal numbers', () => {
+    let ordinalTests = [
+      ['1ere', '1<sup>re</sup>'],
+      ['1ère', '1<sup>re</sup>'],
+      ['2eme', '2<sup>e</sup>'],
+      ['2ème', '2<sup>e</sup>'],
+      ['3eme', '3<sup>e</sup>'],
+      ['3ème', '3<sup>e</sup>'],
+      ['10ème', '10<sup>e</sup>'],
+      ['1ers', '1<sup>ers</sup>'],
+      ['1ères', '1<sup>res</sup>'],
+      ['1eres', '1<sup>res</sup>'],
+      ['2emes', '2<sup>es</sup>'],
+      ['2èmes', '2<sup>es</sup>'],
+      ['10emes', '10<sup>èmes</sup>'],
+      ['10èmes', '10<sup>èmes</sup>']
+    ]
+
+    ordinalTests.forEach(([source, expected]) => {
+      it(`replaces ${source} by ${expected}`, () => {
+        expect(fix(rules, source)).to.eql(expected)
+      })
+    })
+  })
 })
