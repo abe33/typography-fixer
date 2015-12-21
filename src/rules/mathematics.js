3684943d349b7f49d2d0d2b5c19beff4ad61d7e8
abe33 <cedric.nehemie@gmail.com>
2015-12-21 12:25:19 +0100

    Add subtraction and vulgar fraction rules

--- src/rules/mathematics.js
+++ src/rules/mathematics.js
@@ -1,6 +1,10 @@
 import {rule, group} from '../typographic-fixer'
 
 export default group('mathematic', [
-  rule('multiplication', /(\d)\s*x\s*(\d)/, '$1\u00a0\u00D7\u00a0$2'),
-  rule('division', /(\d)\s*\/\s*(\d)/, '$1\u00a0÷\u00a0$2')
+  rule('vulgarFractionOneQuarter', /\b1\s*\/\s*4\b/, '\u00bc'),
+  rule('vulgarFractionOneHalf', /\b1\s*\/\s*2\b/, '\u00bd'),
+  rule('vulgarFractionThreeQuarter', /\b3\s*\/\s*4\b/, '\u00be'),
+  rule('multiplication', /(\d)\s*x\s*(\d)/, '$1\u00a0\u00d7\u00a0$2'),
+  rule('division', /(\d)\s*\/\s*(\d)/, '$1\u00a0\u00f7\u00a0$2'),
+  rule('subtraction', /(\d)\s*-\s*(\d)/, '$1\u00a0\u2212\u00a0$2')
 ])
--- test/rules/mathematics.js
+++ test/rules/mathematics.js
@@ -3,19 +3,42 @@ import {fix} from '../../src/typographic-fixer'
 import rules from '../../src/rules/mathematics'
 
 describe('mathematic rules', () => {
-  it('replaces 12x21 with 12 \u00D7 21 with non-breaking spaces', () => {
-    expect(fix(rules, '12x21')).to.eql('12\u00a0\u00D7\u00a021')
+  it('replaces 12x21 with 12 \u00d7 21 with non-breaking spaces', () => {
+    expect(fix(rules, '12x21')).to.eql('12\u00a0\u00d7\u00a021')
   })
 
-  it('replaces 12 x 21 with 12 \u00D7 21 with non-breaking spaces', () => {
-    expect(fix(rules, '12 x 21')).to.eql('12\u00a0\u00D7\u00a021')
+  it('replaces 12 x 21 with 12 \u00d7 21 with non-breaking spaces', () => {
+    expect(fix(rules, '12 x 21')).to.eql('12\u00a0\u00d7\u00a021')
+  })
+
+  it('replaces 1/2 with \u00bd', () => {
+    expect(fix(rules, '1/2')).to.eql('\u00bd')
+    expect(fix(rules, '1 / 2')).to.eql('\u00bd')
+  })
+
+  it('replaces 1/4 with \u00bc', () => {
+    expect(fix(rules, '1/4')).to.eql('\u00bc')
+    expect(fix(rules, '1 / 4')).to.eql('\u00bc')
+  })
+
+  it('replaces 3/4 with \u00be', () => {
+    expect(fix(rules, '3/4')).to.eql('\u00be')
+    expect(fix(rules, '3 / 4')).to.eql('\u00be')
   })
 
   it('replaces 12/21 with 12 ÷ 21 with non-breaking spaces', () => {
     expect(fix(rules, '12/21')).to.eql('12\u00a0÷\u00a021')
   })
 
-  it('replaces 12 / 21 with 12 ÷ 21 with non-breaking spaces', () => {
-    expect(fix(rules, '12 / 21')).to.eql('12\u00a0÷\u00a021')
+  it('replaces 12 / 21 with 12 \u00f7 21 with non-breaking spaces', () => {
+    expect(fix(rules, '12 / 21')).to.eql('12\u00a0\u00f7\u00a021')
+  })
+
+  it('replaces 12-21 with 12 \u2212 21 with non-breaking spaces', () => {
+    expect(fix(rules, '12-21')).to.eql('12\u00a0\u2212\u00a021')
+  })
+
+  it('replaces 12 - 21 with 12 \u2212 21 with non-breaking spaces', () => {
+    expect(fix(rules, '12 - 21')).to.eql('12\u00a0\u2212\u00a021')
   })
 })
