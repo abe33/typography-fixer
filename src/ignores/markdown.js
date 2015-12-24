40139d46cd5d7a6e96ceff987f0bb38657636bfe
abe33 <cedric.nehemie@gmail.com>
2015-12-25 00:19:34 +0100

    Set multiline flag on markdown rules that require it

--- src/ignores/markdown.js
+++ src/ignores/markdown.js
@@ -3,9 +3,9 @@ import {ignore, group} from '../typography-fixer'
 export default group('markdown', [
   ignore('imageAndLink', /!?\[[^\]]+\]\([^\)]+\)/),
   ignore('imageAndLinkWithExternalDefinition', /!?\[[^\]]+\]\s*\[[^\]]*\]/),
-  ignore('linkDefinition', /\[[^\]]+\]:.*$/),
+  ignore('linkDefinition', /\[[^\]]+\]:.*$/m),
   ignore('codeBlock', /(```)(.|\n)*?\1/),
-  ignore('preformattedBlock', /^\x20{4}.*$/),
+  ignore('preformattedBlock', /^\x20{4}.*$/m),
   ignore('codeInline', /(`{1,2}).*?\1/),
   ignore('url', /\b((?:[a-zA-Z][\w-]+:(?:\/{1,3}|[a-zA-Z0-9%])|www\d{0,3}[.]|[a-zA-Z0-9.\-]+[.][a-zA-Z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/)
 ])
