d8a6ef2de645be2a0f599f7a731a625f38697ac5
abe33 <cedric.nehemie@gmail.com>
2016-01-04 14:36:50 +0100

    Apply paragraph last words non-breaking space only if it's a simple space

--- src/rules/line-breaks.js
+++ src/rules/line-breaks.js
@@ -8,6 +8,6 @@ function createRuleset () {
   return group('line-breaks', [
     rule('numberBeforeWord', /(\d)\x20(\D)/, '$1\u00a0$2'),
     rule('shortWords', /\b(\w{1,3})\x20/, '$1\u00a0'),
-    rule('lastParagraphWords', /(\w+)\s(\w+\.)$/m, '$1\u00a0$2')
+    rule('lastParagraphWords', /(\w+)\x20(\w+\.)$/m, '$1\u00a0$2')
   ])
 }
