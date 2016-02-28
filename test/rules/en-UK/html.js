0374d3d033d42b287aeb6bf72fce7eddfc0ebe81
abe33 <cedric.nehemie@gmail.com>
2016-02-28 02:29:53 +0100

    :fire: Remove unused function and import in tests

--- test/rules/en-UK/html.js
+++ test/rules/en-UK/html.js
@@ -1,9 +1,8 @@
 import expect from 'expect.js'
-import {fix, check} from '../../../src/typography-fixer'
+import {fix} from '../../../src/typography-fixer'
 import rules from '../../../src/rules/en-UK/html'
 
 const fixString = fix(rules)
-const checkString = check(rules)
 
 describe('en-UK html rules', () => {
   it('includes fraction rules', () => {
